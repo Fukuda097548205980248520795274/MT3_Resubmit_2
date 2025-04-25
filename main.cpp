ad738c1a9a19ad14b9f8f5ad6aafc4e971f91535
Fukuda097548205980248520795274 <k024g0083@g.neec.ac.jp>
2025-04-25 11:20:28 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,110 @@
+#include <Novice.h>
+#include "Struct.h"
+#include "Const.h"
+#include "./Func/Vector/Vector.h"
+#include "./Func/Matrix/Matrix.h"
+#include "./Func/Draw/Draw.h"
+#include "imgui.h"
+
+const char kWindowTitle[] = "LE2A_11_フクダソウワ_MT3";
+
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) 
+{
+
+	// ライブラリの初期化
+	Novice::Initialize(kWindowTitle, kWindowWidth, kWindowHeight);
+
+	// キー入力結果を受け取る箱
+	char keys[256] = {0};
+	char preKeys[256] = {0};
+
+
+	/*---------------
+	    変数を作る
+	---------------*/
+
+	// カメラの移動・回転
+	Vector3 cameraTranslate = { 0.0f , 1.9f , -6.49f };
+	Vector3 cameraRotate = { 0.26f , 0.0f , 0.0f };
+
+
+	// 球
+	Sphere sphere;
+	sphere.center = { 0.0f , 0.0f , 0.0f };
+	sphere.radius = 0.5f;
+
+
+	// ウィンドウの×ボタンが押されるまでループ
+	while (Novice::ProcessMessage() == 0) {
+
+		// フレームの開始
+		Novice::BeginFrame();
+
+		// キー入力を受け取る
+		memcpy(preKeys, keys, 256);
+		Novice::GetHitKeyStateAll(keys);
+
+		///
+		/// ↓更新処理ここから
+		///
+
+		ImGui::Begin("Window");
+		ImGui::DragFloat3("cameraTranslate", &cameraTranslate.x, 0.01f);
+		ImGui::DragFloat3("cameraRotate", &cameraRotate.x , 0.01f);
+		ImGui::DragFloat3("SphereCenter", &sphere.center.x, 0.01f);
+		ImGui::DragFloat("SphereRadius", &sphere.radius, 0.01f);
+		ImGui::End();
+
+
+		/*-------------------
+		    座標変換の行列
+		-------------------*/
+
+		// ビュー行列
+		Matrix4x4 viewMatrix = Inverse(MakeAffineMatrix({ 1.0f , 1.0f , 1.0f }, cameraRotate, cameraTranslate));
+
+		// 透視投影行列
+		Matrix4x4 projectionMatrix = MakePerspectiveFovMatrix(0.45f, static_cast<float>(kWindowWidth) / static_cast<float>(kWindowHeight), 0.1f, 100.0f);
+
+		// ビューポート行列
+		Matrix4x4 viewportMatrix = MakeViewportMatrix(0.0f, 0.0f, static_cast<float>(kWindowWidth), static_cast<float>(kWindowHeight), 0.0f, 1.0f);
+
+
+		///
+		/// ↑更新処理ここまで
+		///
+
+		///
+		/// ↓描画処理ここから
+		///
+
+
+		/*----------
+		    図形
+		----------*/
+
+		// グリッド
+		DrawGrid(Multiply(viewMatrix, projectionMatrix), viewportMatrix);
+
+		// 球
+		DrawSphere(sphere, Multiply(viewMatrix, projectionMatrix), viewportMatrix, 0x000000FF);
+
+		
+		///
+		/// ↑描画処理ここまで
+		///
+
+		// フレームの終了
+		Novice::EndFrame();
+
+		// ESCキーが押されたらループを抜ける
+		if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
+			break;
+		}
+	}
+
+	// ライブラリの終了
+	Novice::Finalize();
+	return 0;
+}
